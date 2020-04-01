8cc7233b2ebc8eaf6651acecc6429e77c768ac6a
hecrabbs <hecrabbs@gmail.com>
2020-03-31 18:12:17 -0700

    Fixed Error with newsApi

--- src/NewsApi.js
+++ src/NewsApi.js
@@ -1,5 +1,5 @@
 const API_KEY = `&apiKey=bbdaee4b911644a796017954f3f1362a`;
-const URL = `http://newsapi.org/v2/everything?sortBy=relevancy&q=`;
+const URL = `https://newsapi.org/v2/everything?sortBy=relevancy&q=`;
 
 const searchArticles = search => {
   let req = `${URL}${search}${API_KEY}`;
